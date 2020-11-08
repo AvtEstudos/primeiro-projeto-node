c0e3fdce841067d2f8c3e5409355076af4eeebe1
AvtEstudos <andretamayoshi@gmail.com>
2020-11-08 11:44:48 -0300

    Endereço das imagens

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -25,6 +25,8 @@ class ListProvidersService {
       `providers-list:${user_id}`
     );
 
+    //users = null;
+
     if(!users) {
       users = await this.usersRepository.findAllProvider({
         except_user_id: user_id,
